cdb52a82fbb41b4139ff4cda3649b3365429ba53
Andrei Terentiev <terentiev.andrei@gmail.com>
2017-10-03 22:03:10 +0900

    update-keychain-management

--- O3/Onboarding/WelcomeTableViewController.swift
+++ O3/Onboarding/WelcomeTableViewController.swift
@@ -13,7 +13,6 @@ import KeychainAccess
 class WelcomeTableViewController: UITableViewController {
     @IBOutlet weak var privateKeyQR: UIImageView!
     @IBOutlet weak var privateKeyLabel: UILabel!
-    let keychain = Keychain(service: "network.o3.wallet")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +21,10 @@ class WelcomeTableViewController: UITableViewController {
         self.privateKeyQR.image = UIImage(qrData: Authenticated.account?.wif ?? "", width: self.privateKeyQR.frame.width, height: self.privateKeyQR.frame.height)
         self.privateKeyLabel.text = Authenticated.account?.wif ?? ""
 
+        let keychain = Keychain(service: "network.o3.neo.wallet")
         DispatchQueue.global().async {
             do {
-                try self.keychain
+                try keychain
                     .accessibility(.whenPasscodeSetThisDeviceOnly, authenticationPolicy: .userPresence)
                     .set((Authenticated.account?.wif)!, key: "ozonePrivateKey")
             } catch let error {
@@ -38,24 +38,6 @@ class WelcomeTableViewController: UITableViewController {
     }
 
     @IBAction func startTapped(_ sender: Any) {
-        DispatchQueue.main.async {
-
-        }
-        DispatchQueue.global().async {
-            do {
-                let password = try self.keychain
-                    .authenticationPrompt("Authenticate to view your private key")
-                    .get("ozonePrivateKey")
-
-            } catch let error {
-                fatalError("Unable to store private key in keychain \(error.localizedDescription)")
-            }
-        }
+       self.performSegue(withIdentifier: "segueToMainFromWelcome", sender: nil)
     }
 }
-
-        //DispatchQueue.global().async {
-        //    print (keychain["ozonePrivateKey"])
-            //self.performSegue(withIdentifier: "segueToMainFromWelcome", sender: nil)
-        //}
- //   }
