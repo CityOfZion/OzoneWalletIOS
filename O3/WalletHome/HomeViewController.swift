b379027a6bd14c85127dee591133285f2c4ad965
Apisit Toompakdee <apmotive@gmail.com>
2017-11-05 15:09:34 +0900

    nullable

--- O3/WalletHome/HomeViewController.swift
+++ O3/WalletHome/HomeViewController.swift
@@ -13,12 +13,12 @@ import NeoSwift
 import Channel
 
 class HomeViewController: ThemedViewController, UITableViewDelegate, UITableViewDataSource, GraphPanDelegate, ScrollableGraphViewDataSource {
-    
+
     // Settings for price graph interval
     @IBOutlet weak var walletHeaderCollectionView: UICollectionView!
-    
+
     @IBOutlet weak var assetsTable: UITableView!
-    
+
     // Xcode 9 beta issue with outlet connections gonna just hook up two buttons for now
     @IBOutlet weak var fiveMinButton: UIButton!
     @IBOutlet weak var fifteenMinButton: UIButton!
@@ -26,13 +26,13 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
     @IBOutlet weak var sixtyMinButton: UIButton!
     @IBOutlet weak var oneDayButton: UIButton!
     @IBOutlet weak var allButton: UIButton!
-    
+
     @IBOutlet weak var graphViewContainer: UIView!
-    
+
     @IBOutlet var activatedLineLeftConstraint: NSLayoutConstraint?
     var group: DispatchGroup?
     @IBOutlet weak var activatedLine: UIView!
-    
+
     var graphView: ScrollableGraphView!
     var portfolio: PortfolioValue?
     var selectedInterval: PriceInterval = .fifteenMinutes
@@ -40,29 +40,29 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
     var panView: GraphPanView!
     var selectedAsset = "neo"
     var firstTimeGraphLoad = true
-    
+
     var writeableNeoBalance = 0
     var writeableGasBalance = 0.0
-    
+
     var readOnlyNeoBalance = 0
     var readOnlyGasBalance = 0.0
-    
+
     var selectedPrice: PriceData?
     var referenceCurrency: Currency = .btc
-    
+
     func addThemedElements() {
         themedTableViews = [assetsTable]
         themedCollectionViews = [walletHeaderCollectionView]
         themedTransparentButtons = [fiveMinButton, fifteenMinButton, thirtyMinButton, sixtyMinButton, oneDayButton, allButton]
         themedBackgroundViews = [graphViewContainer]
     }
-    
+
     var portfolioType: PortfolioType = .writable {
         didSet {
             self.loadPortfolio()
         }
     }
-    
+
     var displayedNeoAmount: Int {
         switch portfolioType {
         case .readOnly:
@@ -73,7 +73,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             return readOnlyNeoBalance + writeableNeoBalance
         }
     }
-    
+
     var displayedGasAmount: Double {
         switch portfolioType {
         case .readOnly:
@@ -84,7 +84,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             return readOnlyGasBalance + writeableGasBalance
         }
     }
-    
+
     func loadWatchAddresses() -> [WatchAddress] {
         do {
             let watchAddresses: [WatchAddress] = try
@@ -94,7 +94,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             return []
         }
     }
-    
+
     func loadPortfolio() {
         O3Client.shared.getPortfolioValue(self.displayedNeoAmount, gas: self.displayedGasAmount, interval: self.selectedInterval.rawValue) {result in
             switch result {
@@ -115,7 +115,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             }
         }
     }
-    
+
     func loadBalanceData(fromReadOnly: Bool, address: String) {
         Neo.client.getAccountState(for: address) { result in
             switch result {
@@ -142,7 +142,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             }
         }
     }
-    
+
     /*
      * We simulatenously load all of your balance data at once
      * Get the sum of your read only addresses and then the hot wallet as well
@@ -158,18 +158,18 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             group?.enter()
             loadBalanceData(fromReadOnly: false, address: address)
         }
-        
+
         for watchAddress in self.loadWatchAddresses() {
             self.group?.enter()
             self.loadBalanceData(fromReadOnly: true, address: watchAddress.address!)
         }
-        
+
         group?.notify(queue: .main) {
             self.tabBarController?.tabBar.isUserInteractionEnabled = true
             self.loadPortfolio()
         }
     }
-    
+
     @objc func updateGraphAppearance(_ sender: Any) {
         DispatchQueue.main.async {
             self.graphView.removeFromSuperview()
@@ -178,49 +178,49 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             self.loadPortfolio()
         }
     }
-    
+
     func setupGraphView() {
         graphView = ScrollableGraphView.ozoneTheme(frame: graphViewContainer.bounds, dataSource: self)
         graphViewContainer.embed(graphView)
-        
+
         panView = GraphPanView(frame: graphViewContainer.bounds)
         panView.delegate = self
         graphViewContainer.embed(panView)
     }
-    
+
     func panDataIndexUpdated(index: Int, timeLabel: UILabel) {
         DispatchQueue.main.async {
             self.selectedPrice = self.portfolio?.data.reversed()[index]
             self.walletHeaderCollectionView.reloadData()
-            
+
             let posixString = self.portfolio?.data.reversed()[index].time ?? ""
             timeLabel.text = posixString.intervaledDateString(self.selectedInterval)
             timeLabel.sizeToFit()
         }
     }
-    
+
     func panEnded() {
         selectedPrice = self.portfolio?.data.first
         walletHeaderCollectionView.reloadData()
     }
-    
+
     func addObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(self.getBalance), name: Notification.Name("ChangedNetwork"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.getBalance), name: Notification.Name("UpdatedWatchOnlyAddress"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateGraphAppearance), name: Notification.Name("ChangedTheme"), object: nil)
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self, name: Notification.Name("ChangedNetwork"), object: nil)
         NotificationCenter.default.removeObserver(self, name: Notification.Name("UpdatedWatchOnlyAddress"), object: nil)
         NotificationCenter.default.removeObserver(self, name: Notification.Name("ChangedTheme"), object: nil)
     }
-    
+
     override func viewDidLoad() {
         addThemedElements()
         super.viewDidLoad()
         addObservers()
-        
+
         if UserDefaults.standard.string(forKey: "subscribedAddress") != Authenticated.account?.address {
             Channel.shared().unsubscribe(fromTopic: "*") {
                 Channel.shared().subscribe(toTopic: (Authenticated.account?.address)!)
@@ -228,28 +228,28 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
                 UserDefaults.standard.synchronize()
             }
         }
-        
+
         walletHeaderCollectionView.delegate = self
         walletHeaderCollectionView.dataSource = self
         assetsTable.delegate = self
         assetsTable.dataSource = self
         assetsTable.tableFooterView = UIView(frame: .zero)
-        
+
         //control the size of the graph area here
         self.assetsTable.tableHeaderView?.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height * 0.5)
         setupGraphView()
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.getBalance()
     }
-    
+
     /*
      * Although we have only two assets right now we expect the asset list to be of arbitrary
      * length as new tokens are introduced, we must be flexible enough to support that
      */
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             guard let cell = assetsTable.dequeueReusableCell(withIdentifier: "neoAssetCell") as? NeoAssetCell else {
@@ -259,7 +259,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
                 let firstPrice = portfolio?.firstPrice["neo"] else {
                     return UITableViewCell()
             }
-            
+
             cell.data = NeoAssetCell.Data(amount: displayedNeoAmount,
                                           referenceCurrency: referenceCurrency,
                                           latestPrice: latestPrice,
@@ -270,12 +270,12 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
         guard let cell = assetsTable.dequeueReusableCell(withIdentifier: "gasAssetCell") as? GasAssetCell else {
             fatalError("undefined table view behavior")
         }
-        
+
         guard let latestPrice = portfolio?.price["gas"],
             let firstPrice = portfolio?.firstPrice["gas"] else {
                 return UITableViewCell()
         }
-        
+
         cell.data = GasAssetCell.Data(amount: displayedGasAmount,
                                       referenceCurrency: referenceCurrency,
                                       latestPrice: latestPrice,
@@ -283,7 +283,7 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
         cell.selectionStyle = .none
         return cell
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segueToAssetDetail" {
             guard let dest = segue.destination as? AssetDetailViewController else {
@@ -292,22 +292,22 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             dest.selectedAsset = self.selectedAsset
         }
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedAsset = indexPath.row == 0 ? "neo": "gas"
         DispatchQueue.main.async {
             self.performSegue(withIdentifier: "segueToAssetDetail", sender: nil)
         }
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-    
+
     @IBAction func tappedIntervalButton(_ sender: UIButton) {
         DispatchQueue.main.async {
             self.view.needsUpdateConstraints()
@@ -320,21 +320,21 @@ class HomeViewController: ThemedViewController, UITableViewDelegate, UITableView
             })
         }
     }
-    
+
     // MARK: - Graph delegate
     func value(forPlot plot: Plot, atIndex pointIndex: Int) -> Double {
         // Return the data for each plot.
-        
+
         if pointIndex > portfolio!.data.count {
             return 0
         }
         return referenceCurrency == .btc ? portfolio!.data.reversed()[pointIndex].averageBTC : portfolio!.data.reversed()[pointIndex].averageUSD
     }
-    
+
     func label(atIndex pointIndex: Int) -> String {
         return ""//String(format:"%@",portfolio!.data[pointIndex].time)
     }
-    
+
     func numberOfPoints() -> Int {
         if portfolio == nil {
             return 0
@@ -347,7 +347,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return portfolio == nil ? 0 : 3
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "walletHeaderCollectionCell", for: indexPath) as? WalletHeaderCollectionCell else {
             fatalError("Undefined collection view behavior")
@@ -363,12 +363,12 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             portfolioType = .readOnlyAndWritable
         default: fatalError("Undefined wallet header cell")
         }
-        
+
         guard let latestPrice = selectedPrice,
             let previousPrice = portfolio?.data.last else {
                 fatalError("Undefined wallet header cell")
         }
-        
+
         let data = WalletHeaderCollectionCell.Data (
             portfolioType: portfolioType,
             index: indexPath.row,
@@ -380,29 +380,29 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         cell.data = data
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
         let screenSize = UIScreen.main.bounds
         return CGSize(width: screenSize.width, height: 75)
     }
-    
+
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         var visibleRect = CGRect()
-        
+
         visibleRect.origin = walletHeaderCollectionView.contentOffset
         visibleRect.size = walletHeaderCollectionView.bounds.size
-        
+
         let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
-        
-        let visibleIndexPath: IndexPath = walletHeaderCollectionView.indexPathForItem(at: visiblePoint)!
-        
+
+        let visibleIndexPath: IndexPath? = walletHeaderCollectionView.indexPathForItem(at: visiblePoint)
+
         if visibleIndexPath != nil {
-            self.portfolioType = self.indexToPortfolioType(visibleIndexPath.row)
+            self.portfolioType = self.indexToPortfolioType(visibleIndexPath!.row)
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         switch referenceCurrency {
         case .btc:
@@ -414,7 +414,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         assetsTable.reloadData()
         graphView.reload()
     }
-    
+
     func indexToPortfolioType(_ index: Int) -> PortfolioType {
         switch index {
         case 0:
@@ -427,14 +427,14 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             fatalError("Invalid Portfolio Index")
         }
     }
-    
+
     func didTapLeft(index: Int, portfolioType: PortfolioType) {
         DispatchQueue.main.async {
             self.walletHeaderCollectionView.scrollToItem(at: IndexPath(row: index - 1, section: 0), at: .left, animated: true)
             self.portfolioType = self.indexToPortfolioType(index - 1)
         }
     }
-    
+
     func didTapRight(index: Int, portfolioType: PortfolioType) {
         DispatchQueue.main.async {
             self.walletHeaderCollectionView.scrollToItem(at: IndexPath(row: index + 1, section: 0), at: .right, animated: true)
