4cfc1c7a0eb1965c536d010d74324490d167b53b
andrei <terentiev.andrei@gmail.com>
2018-03-23 11:08:55 +0900

    Featured news (#69)
    * bump version number
    
    * finish featured content
    
    * Update FeatureFeed.swift
    
    * refine, inapp-browser with share
    
    * disable user interaction button

--- O3/NewsFeed/NewsFeedViewController.swift
+++ O3/NewsFeed/NewsFeedViewController.swift
@@ -9,10 +9,14 @@
 import Foundation
 import UIKit
 import Kingfisher
+import WebBrowser
 
-class NewsFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class NewsFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
     @IBOutlet weak var newsFeedTableView: UITableView!
+    @IBOutlet weak var featuredCollectionView: UICollectionView!
     var feedData: FeedData?
+    var featureData: FeatureFeed?
     var urlToLoad = ""
 
     func setThemedElements() {
@@ -23,27 +27,53 @@ class NewsFeedViewController: UIViewController, UITableViewDelegate, UITableView
 
     }
 
+    func loadNews() {
+        O3Client.shared.getFeatures { result in
+            switch result {
+            case .failure:
+                return
+            case .success(let featureData):
+                DispatchQueue.main.async {
+                    self.navigationItem.largeTitleDisplayMode = .always
+                    if featureData.features.count == 0 {
+                        self.newsFeedTableView.tableHeaderView?.frame.size = CGSize(width: self.newsFeedTableView.frame.width, height: 0)
+                    } else {
+                        let headerHeight = (self.newsFeedTableView.frame.width * 9 / 16) + 45
+                        self.newsFeedTableView.tableHeaderView?.frame.size = CGSize(width: self.newsFeedTableView.frame.width, height: headerHeight)
+                    }
+                }
+                self.featureData = featureData
+                O3Client.shared.getNewsFeed { result in
+                    switch result {
+                    case .failure:
+                        return
+                    case .success(let feedData):
+                        self.feedData = feedData
+                        DispatchQueue.main.async {
+                            self.featuredCollectionView.reloadData()
+                            self.newsFeedTableView.reloadData()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setThemedElements()
         newsFeedTableView.dataSource = self
         newsFeedTableView.delegate = self
+        featuredCollectionView.dataSource = self
+        featuredCollectionView.delegate = self
         newsFeedTableView.tableFooterView = UIView(frame: .zero)
 
+        featuredCollectionView.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+
+        self.edgesForExtendedLayout = UIRectEdge.bottom
         self.navigationController?.hideHairline()
-        self.navigationItem.largeTitleDisplayMode = .automatic
         self.navigationItem.title = "News Feed"
-        O3Client.shared.getNewsFeed { result in
-            switch result {
-            case .failure:
-                return
-            case .success(let feedData):
-                self.feedData = feedData
-                DispatchQueue.main.async {
-                    self.newsFeedTableView.reloadData()
-                }
-            }
-        }
+        loadNews()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,9 +92,65 @@ class NewsFeedViewController: UIViewController, UITableViewDelegate, UITableView
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         let item = feedData?.items[indexPath.row]
-        urlToLoad = item?.link ?? ""
-        self.performSegue(withIdentifier: "segueToNewsItem", sender: nil)
+        if item?.link == nil {
+            return
+        }
+        let webBrowserViewController = WebBrowserViewController()
+        webBrowserViewController.hidesBottomBarWhenPushed = true
+        webBrowserViewController.tintColor = Theme.light.primaryColor
+        webBrowserViewController.loadURLString((item?.link)!)
+        self.navigationController?.pushViewController(webBrowserViewController, animated: true)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return featureData?.features.count ?? 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let featureItem = featureData?.features[indexPath.row] else {
+            fatalError("Undefined Selection Behavior")
+        }
+        if let link = URL(string: featureItem.actionURL) {
+            let webBrowserViewController = WebBrowserViewController()
+            webBrowserViewController.tintColor = Theme.light.primaryColor
+            webBrowserViewController.loadURL(link)
+            let navigationWebBrowser = WebBrowserViewController.rootNavigationWebBrowser(webBrowser: webBrowserViewController)
+            present(navigationWebBrowser, animated: true, completion: nil)
+        }
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featuredCollectionCell", for: indexPath) as? FeaturedCollectionCell,
+            let featureItem = featureData?.features[indexPath.row] else {
+            fatalError("Undefined CollectionView Behavior")
+        }
+
+        cell.featuredImage.kf.setImage(with: URL(string: featureItem.imageURL))
+        cell.titleLabel.text = featureItem.title.uppercased()
+        cell.subtitleLabel.text = featureItem.subtitle
+        cell.actionButton.setTitle(featureItem.actionTitle.uppercased(), for: .normal)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenSize = UIScreen.main.bounds
+        return CGSize(width: screenSize.width - 48, height: (newsFeedTableView.frame.width * 9.0 / 16.0) + 45)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if section == 0 {
+                return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
